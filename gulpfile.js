c85da9a2f9556a16422af39292549f3497804f5a
Nick Jenkins <nickjenkinsweb@gmail.com>
2016-10-04 16:46:45 -0400

    Added browserify to bundle src files into umd modules in lib folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var streamify = require('gulp-streamify');
 var concat = require('gulp-concat');
 var source = require('vinyl-source-stream');
 var mocha = require('gulp-mocha');
 
 var package = require('./package.json');
 
-gulp.task('test', function(){
+gulp.task('test', function () {
   return gulp.src(['./spec/*.spec.js'], {read: false})
     .pipe(mocha({
       reporter: 'min'
@@ -14,13 +17,21 @@ gulp.task('test', function(){
 });
 
 gulp.task('build', function () {
-  gulp.src(['./src/index.js'])
-    .pipe(uglify())
-    .pipe(concat(package.name + '.min.js'))
+  return browserify('./src/index.js', {
+    standalone: package.name
+  }).bundle()
+    .pipe(source(package.name + '.js'))
+    .pipe(gulp.dest('./lib'))
+    .pipe(rename(package.name + '.min.js'))
+    .pipe(streamify(uglify()))
     .pipe(gulp.dest('./lib'));
+  // gulp.src(['./src/index.js'])
+  //   .pipe(uglify())
+  //   .pipe(concat(package.name + '.min.js'))
+  //   .pipe(gulp.dest('./lib'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch('./*.js', ['test']);
   gulp.watch('./spec/*.js', ['test']);
 });
