9c8084e400368aa059a611dbec06c179d4db9a80
Nick Jenkins <nickjenkinsweb@gmail.com>
2016-10-04 15:51:46 -0400

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var source = require('vinyl-source-stream');
+var mocha = require('gulp-mocha');
+
+var package = require('./package.json');
+
+gulp.task('test', function(){
+  return gulp.src(['./spec/*.spec.js'], {read: false})
+    .pipe(mocha({
+      reporter: 'min'
+    }));
+});
+
+gulp.task('build', function () {
+  gulp.src(['./' + package.name + '.js'])
+    .pipe(uglify())
+    .pipe(concat(package.name + '.min.js'))
+    .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./*.js', ['test']);
+  gulp.watch('./spec/*.js', ['test']);
+});
+
+gulp.task('default', ['build'], function () {});
