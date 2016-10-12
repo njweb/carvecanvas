cae62f146229c35fd8492cda25d537292b9091a8
Nick Jenkins <nickjenkinsweb@gmail.com>
2016-10-12 17:22:46 -0400

    Updated testing tools/tasks to work with es6 js files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,38 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var uglify = require('gulp-uglify');
+var babelify = require('babelify');
+var babel = require('babel-register');
 var rename = require('gulp-rename');
 var streamify = require('gulp-streamify');
 var concat = require('gulp-concat');
-var source = require('vinyl-source-stream');
 var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
 
-var package = require('./package.json');
+var pkg = require('./package.json');
 
-gulp.task('test', function () {
-  return gulp.src(['./spec/*.spec.js'], {read: false})
-    .pipe(mocha({
-      reporter: 'min'
-    }));
-});
-
-gulp.task('build', function () {
+gulp.task('build_libs', function () {
   return browserify('./src/index.js', {
-    standalone: package.name
-  }).bundle()
-    .pipe(source(package.name + '.js'))
+    standalone: pkg.name
+  })
+    .transform('babelify', {presets: ['es2015']})
+    .bundle()
+    .pipe(source(pkg.name + '.js'))
     .pipe(gulp.dest('./lib'))
-    .pipe(rename(package.name + '.min.js'))
+    .pipe(rename(pkg.name + '.min.js'))
     .pipe(streamify(uglify()))
     .pipe(gulp.dest('./lib'));
-  // gulp.src(['./src/index.js'])
-  //   .pipe(uglify())
-  //   .pipe(concat(package.name + '.min.js'))
-  //   .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('spec', function () {
+  return gulp.src(['./spec/*.spec.js'], {read: false})
+    .pipe(mocha({reporter: 'min', compilers: {js: babel}}));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./*.js', ['test']);
-  gulp.watch('./spec/*.js', ['test']);
+  gulp.watch('./*.js', ['spec']);
+  gulp.watch('./spec/*.js', ['spec']);
 });
 
 gulp.task('default', ['build'], function () {});
